6f96835ed8d8b45b90c1c9bb0b327910a0e70331
Jesús López <71353275+LopezJesus@users.noreply.github.com>
2020-10-23 14:06:48 -0700

    Create tipos3.c

---
+++ programas/tipos3.c
@@ -0,0 +1,22 @@
+    @@
+    @@ Instituto Tecnologico de Tijuana
+    @@ Depto de Sistemas y Computación
+    @@ Ing. En Sistemas Computacionales
+    @@   
+    @@ Autor : Jesús Xavier López Galaz  @LopezJesus
+    @@ Repositorio: http://github.com/LopezJesus
+    @@ Fecha de elaboracion: 23/10/2020
+    @@ 
+    @
+    @ Objetivo del programa:
+    @    Aprender a reconocer como funciona la compilacion
+    @
+    
+# include <stdio.h>
+
+void main ( void ){
+  int i;
+  for ( i= 0; i <5; i++ ){
+  printf ( " %d\n " , i );
+  }
+}
