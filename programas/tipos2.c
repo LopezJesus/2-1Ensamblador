3fd41e50df5ededb24da61389eac09c32410a9b6
Jesús López <71353275+LopezJesus@users.noreply.github.com>
2020-10-23 14:04:44 -0700

    Create tipos2.c

---
+++ programas/tipos2.c
@@ -0,0 +1,26 @@
+    @@
+    @@ Instituto Tecnologico de Tijuana
+    @@ Depto de Sistemas y Computación
+    @@ Ing. En Sistemas Computacionales
+    @@   
+    @@ Autor : Jesús Xavier López Galaz  @LopezJesus
+    @@ Repositorio: http://github.com/LopezJesus
+    @@ Fecha de elaboracion: 23/10/2020
+    @@ 
+    @
+    @ Objetivo del programa:
+    @    Aprender a reconocer como funciona el if en c
+    @
+
+# include <stdio.h>
+
+void main ( void ){
+
+int a , b;
+if( a == b ){
+/* Código entonces */
+}
+else {
+/* Código sino */
+}
+}
