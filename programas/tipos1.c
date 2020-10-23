808533552fa0767a574f131966a8bb3f73d8ffad
Jesús López <71353275+LopezJesus@users.noreply.github.com>
2020-10-23 14:03:43 -0700

    Create tipos1.c

---
+++ programas/tipos1.c
@@ -0,0 +1,31 @@
+    @@
+    @@ Instituto Tecnologico de Tijuana
+    @@ Depto de Sistemas y Computación
+    @@ Ing. En Sistemas Computacionales
+    @@   
+    @@ Autor : Jesús Xavier López Galaz  @LopezJesus
+    @@ Repositorio: http://github.com/LopezJesus
+    @@ Fecha de elaboracion: 23/10/2020
+    @@ 
+    @
+    @ Objetivo del programa:
+    @    Aprender a reconocer como funciona el codigo de C, con estructuras de control
+    @
+    
+# include <stdio.h>
+
+void main ( void )
+{
+  int vi , vf , i;
+  for ( i = vi; i <= vf; i++ )
+  {
+    /* Cuerpo del bucle */
+  }
+  
+  i= vi;
+  while ( i <= vf )
+  {
+  /* Cuerpo del bucle */
+    i++;
+  }
+}
